13209dadf193ae34488c2e7b2016cda367846c55
AcademyAspNet <elprofesor.espana@gmail.com>
2025-11-16 14:47:01 +0300

    Added validation

--- Models/CompanyDto.cs
+++ Models/CompanyDto.cs
@@ -4,6 +4,9 @@ namespace Employee_Accounting_Service.Models
 {
     public class CompanyDto
     {
+        [Required(ErrorMessage = "Укажите имя компании")]
+        [MinLength(3, ErrorMessage = "Имя слишком короткое")]
+        [MaxLength(100, ErrorMessage = "Имя компании слишком длинное")]
         public string? Name { get; set; }
     }
 }
